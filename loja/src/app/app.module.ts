7ae14e4f7242a59677b0731807ef4952e31c9c53
Ivirson Daltro <ivi.daltro@gmail.com>
2024-11-01 21:59:45 -0300

    Aula 05 - Pipes

--- loja/src/app/app.module.ts
+++ loja/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './commons/components/footer/footer.component';
 import { HeaderComponent } from './commons/components/header/header.component';
+import { DocumentPipe } from './pipes/document.pipe';
 import { CardComponent } from './shared/components/card/card.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, CardComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CardComponent,
+    DocumentPipe,
+  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
