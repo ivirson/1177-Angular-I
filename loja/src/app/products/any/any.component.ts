390e22e51fb8bcde21ff380e46322823cead0e4e
Ivirson Daltro <ivi.daltro@gmail.com>
2024-11-13 19:55:17 -0300

    Atualizacao v16 > v17 & convert to standalone

---
+++ loja/src/app/products/any/any.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-any',
+  standalone: true,
+  imports: [],
+  templateUrl: './any.component.html',
+  styleUrl: './any.component.css'
+})
+export class AnyComponent {
+
+}
--- loja/src/app/shared/components/card/card.component.ts
+++ loja/src/app/shared/components/card/card.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Product } from 'src/app/models/product.model';
 import { CartService } from 'src/app/services/cart.service';
+import { LowerCasePipe, CurrencyPipe } from '@angular/common';
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css'],
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.css'],
+    standalone: true,
+    imports: [LowerCasePipe, CurrencyPipe],
 })
 export class CardComponent implements OnChanges {
   @Input() product!: Product;
